160acf53ec81b612d433894e5a817343aa6462de
fredo09 <fred_15@outlook.com>
2025-05-21 22:28:24 -0600

    agregar módulo de coches con controlador y pruebas unitarias

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('cars') //! -> decorador para indicar que es un controlador
+export class CarsController {
+	private cars = ['Mazda', 'Toyota', 'jeep'];
+
+	@Get() //! -> decorador para indicar que este servicio sera de un tipo get
+	getAllCars() {
+		console.log("🚀 ~ hemos traido este valor aqui: ", this.cars);
+		return this.cars;
+	}
+
+	//! @Params -> decorador para recibir un parametro en la url endPoint
+	@Get(':id')
+	getCarById( @Param('id') id: string) {
+		console.log({
+			id
+		});
+		return  {
+			id
+		}
+	}
+}
