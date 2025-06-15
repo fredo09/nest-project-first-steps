22dbba9b148344d4ea9ef0548a7255799268e55c
fredo09 <fred_15@outlook.com>
2025-06-14 22:17:12 -0600

    refactor: implementar UUID en el controlador y servicio de marcas, agregar validaciones en DTO de creación CRUD

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -2,25 +2,69 @@ import { Injectable } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
 
+import { v4 as uuid } from 'uuid';
+import { Brand } from './entities/brand.entity';
+
 @Injectable()
 export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name:'Toyota',
+      description: 'This is a sample brand description',
+      createdAt: new Date().getTime()
+    }
+  ];
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+    const { name, description = 'Description por default brand' } = createBrandDto;
+
+    const newBrand: Brand = {
+      id: uuid(),
+      name: name.toLocaleLowerCase(),
+      description: description.toLocaleLowerCase(),
+      createdAt: new Date().getTime()
+    };
+
+    this.brands.push(newBrand);
+
+    return newBrand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find(brand => brand.id === id);
+    if (!brand) 
+      throw new Error(`Brand with id ${id} not found`);
+
+    return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brand = this.findOne(id);
+
+    this.brands = this.brands.map(brand => {
+      if (brand.id === id) {
+        brand = {
+          ...brand,
+          ...updateBrandDto,
+          updatedAt: new Date().getTime()
+        }
+        return brand;
+      }
+      return brand;
+    });
+    return {
+      brand,
+      message: `Brand with id ${id} updated successfully`
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    this.brands = this.brands.filter(brand => brand.id !== id);
   }
 }
--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,1 +1,11 @@
-export class CreateBrandDto {}
+import { IsString, MinLength } from "class-validator";
+
+export class CreateBrandDto {
+	@IsString()
+	@MinLength(3)
+	name: string;
+
+	@IsString()
+	@MinLength(10)
+	description?: string;
+}
